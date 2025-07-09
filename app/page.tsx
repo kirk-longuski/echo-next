145ff67facf74fa84d2517c50323ea6d8f5004c8
Kirk Longuski <longuskikirk@gmail.com>
2025-07-09 15:04:31 -0400

    Test lambda

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,34 @@
 "use client";
-
+import { fetchAuthSession } from 'aws-amplify/auth';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import Lambda from 'aws-sdk/clients/lambda';
-import outputs from "@/amplify_outputs.json";
+import outputs from '../amplify-outputs.json'
+
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
+
+const invocation = async () => {
+  const { credentials } = await fetchAuthSession();
+  const client = new LambdaClient({
+    credentials,
+    region: outputs.auth.aws_region,
+  });
+  const cmd = new InvokeCommand({
+    FunctionName: 'Kirktest',
+    Payload: '{}',
+  });
+  const resp = await client.send(cmd);
+  const body = JSON.parse(new TextDecoder().decode(resp.Payload!));
+  console.log(body);
+}
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
@@ -40,7 +57,7 @@ export default function App() {
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
-      <button onClick={testFunc}>+ old</button>
+      <button onClick={invocation}>+ old</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
